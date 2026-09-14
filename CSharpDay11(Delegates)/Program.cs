d8efaf942c0ff76d900518f76ea22b42cf9a4f23
Jahzeel Kiel <jhzlklprs@gmail.com>
2026-09-14 14:50:36 +0800

    Done some exercises in Delegates

--- CSharpDay11(Delegates)/Program.cs
+++ CSharpDay11(Delegates)/Program.cs
@@ -1,80 +1,97 @@
 ﻿//CSharpFundamentals — Day 11: Delegates
 
-class Program
-{
-    delegate void MyDelegate();
-    static void SayHello()
-    {
-        Console.WriteLine("Hello, C#!");
-    }
+#region -- DELEGATES --
+// class Program
+// {
+//     delegate void MyDelegate();
+//     static void SayHello()
+//     {
+//         Console.WriteLine("Hello, C#!");
+//     }
 
-    delegate void GreetDelegate(string name);
-    static void Greet(string name)
-    {
-        Console.WriteLine($"Hello, {name}!");
-    }
+//     delegate void GreetDelegate(string name);
+//     static void Greet(string name)
+//     {
+//         Console.WriteLine($"Hello, {name}!");
+//     }
 
-    delegate int MathDelegate(int a, int b);
-    static int Add(int a, int b)
-    {
-        return a + b;
-    }
+//     delegate int MathDelegate(int a, int b);
+//     static int Add(int a, int b)
+//     {
+//         return a + b;
+//     }
 
-    delegate int Compute(int a, int b);
-    static int Multiply(int a, int b)
-    {
-        return a * b;
-    }
+//     delegate int Compute(int a, int b);
+//     static int Multiply(int a, int b)
+//     {
+//         return a * b;
+//     }
 
-    static int Subtract(int a, int b)
-    {
-        return a - b;
-    }
-
-
-    static void Main()
-    {
-        #region --Mini Challenge 1 — Your First Delegate--
-
-        //MyDelegate action = SayHello;
-
-        //action();
-
-        #endregion
+//     static int Subtract(int a, int b)
+//     {
+//         return a - b;
+//     }
 
 
-        #region --Mini Challenge 2 — Delegate With Parameters--
-        //GreetDelegate greeting = Greet;
+//     static void Main()
+//     {
+//         #region --Mini Challenge 1 — Your First Delegate--
 
-        //greeting("Kiel");
-        #endregion
+//         //MyDelegate action = SayHello;
 
+//         //action();
 
-        #region --Mini Challenge 3 — Delegate With Return Value--
-        //MathDelegate operation = Add;
-
-        //int result = operation(10, 5);
-        //Console.WriteLine(result);
-        #endregion
+//         #endregion
 
 
-        #region --Mini Challenge 4 — Your Turn--
-        Compute multi = Multiply; 
-        Compute subs = Subtract;
+//         #region --Mini Challenge 2 — Delegate With Parameters--
+//         //GreetDelegate greeting = Greet;
 
-        int product = multi(10, 5);
-        Console.WriteLine(product);
-
-        int difference = subs(10, 5);
-        Console.WriteLine(difference);
-
-        #endregion
-
-    }
-
-}
+//         //greeting("Kiel");
+//         #endregion
 
 
+//         #region --Mini Challenge 3 — Delegate With Return Value--
+//         //MathDelegate operation = Add;
+
+//         //int result = operation(10, 5);
+//         //Console.WriteLine(result);
+//         #endregion
 
 
+//         #region --Mini Challenge 4 — Your Turn--
+//         Compute multi = Multiply; 
+//         Compute subs = Subtract;
+
+//         int product = multi(10, 5);
+//         Console.WriteLine(product);
+
+//         int difference = subs(10, 5);
+//         Console.WriteLine(difference);
+
+//         #endregion
+
+//     }
+
+// }
+
+#endregion
+
+#region -- DELEGATES PRACTICE --
+
+// class Program
+// {
+//     static void Main()
+//     {
+//         Console.WriteLine(add(5, 10));
+//     }
+
+//     delegate int Operation (int a, int b);
+
+//     static Operation add = Add;
+
+//     static int Add (int a, int b) => a + b;
+// }
+
+#endregion
 
