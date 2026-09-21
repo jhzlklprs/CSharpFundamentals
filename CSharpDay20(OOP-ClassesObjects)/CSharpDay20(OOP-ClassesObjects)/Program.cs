d11b5e5cb65a7dc910ae4e2b1dc92a61a1d3ab49
Jahzeel Kiel <jhzlklprs@gmail.com>
2026-09-21 16:11:31 +0800

    New folder OOP

---
+++ CSharpDay20(OOP-ClassesObjects)/CSharpDay20(OOP-ClassesObjects)/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace CSharpDay20_OOP_ClassesObjects_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
