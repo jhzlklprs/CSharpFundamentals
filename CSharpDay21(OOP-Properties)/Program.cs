e4279c589cce5157256fcad2a682e1d553f83d5d
Jahzeel Kiel <jhzlklprs@gmail.com>
2026-09-22 17:24:11 +0800

    Update OOP Properties exercises

--- CSharpDay21(OOP-Properties)/Program.cs
+++ CSharpDay21(OOP-Properties)/Program.cs
@@ -2,9 +2,69 @@
 {
     internal class Program
     {
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //Mission: Rebuild Book using properties instead of plain public fields.
+            Book myBook = new Book();
+            myBook.Title = "How to train your dragon";
+            myBook.Author = "John Doe";
+            myBook.Pages = -50;
+
+            myBook.DisplayInfo();
+            myBook.Checkout();
         }
+
+
+        class Book
+        {
+            public string Title { get; set; }
+            public string Author { get; set; }
+            public bool IsCheckedOut { get; private set; }
+           
+            private int pages;
+            public int Pages
+            {
+                get { return pages; }
+                set
+                {
+                    if (value < 0)
+                    {
+                        Console.WriteLine("Pages can't be negative.");
+                    }
+                    else
+                    {
+                        pages = value;
+                    }
+                }
+            }
+
+
+            public void DisplayInfo()
+            {
+                Console.WriteLine($"Title: {Title}");
+                Console.WriteLine($"Author: {Author}");
+                Console.WriteLine($"Pages: {Pages}");
+                Console.WriteLine($"Checked Out: {IsCheckedOut}\n");
+
+            }
+
+
+
+            public void Checkout()
+            {
+                if (IsCheckedOut)
+                {
+                    Console.WriteLine($"{Title} is already checked out.");
+                }
+                else
+                {
+                    IsCheckedOut = true;   // <- the actual action happens here
+                    Console.WriteLine($"{Title} has been checked out.");
+                }
+            }
+
+        }
+
     }
 }
