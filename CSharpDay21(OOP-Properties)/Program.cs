07a8a55c8b23f8ecb75e565f17ef34a4e1a3da0c
Jahzeel Kiel <jhzlklprs@gmail.com>
2026-09-22 16:12:51 +0800

    New Folder Day 21

---
+++ CSharpDay21(OOP-Properties)/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace CSharpDay21_OOP_Properties_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
